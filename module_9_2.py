fad68642ad62d4df07aa4fda1bc8b6529f92c435
Infobest37 <147615831+Infobest37@users.noreply.github.com>
2024-10-03 17:27:44 +0300

    Update module_9_2.py

--- module_9_2.py
+++ module_9_2.py
@@ -3,7 +3,7 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 
 first_result = [len(x) for x in first_strings if len(x) > 5 ]
 second_result =[(x,y) for x in first_strings for y in second_strings if len(x) == len(y)]
-third_result ={**{x:len(x) for x in first_strings  if len(x) % 2 == 0}, # ** распаковывает словарь и объеденяет их в один по очереди согласно как написано.
+third_result ={**{x:len(x) for x in first_strings  if len(x) % 2 == 0}, # ** распаковывает словарь и объеденяет их в один по очереди согласно тому как написан код.
                **{y: len(y) for y in second_strings if len(y) % 2 == 0}}
 print(first_result)
 print(second_result)
